5501f1af8b2d4337fce62e5d95c76995d908498e
UrmatBolotbek <urmar9797@gmail.com>
2025-02-07 21:39:50 +0100

    add PrefixSum

---
+++ src/PrefixSum.java
@@ -0,0 +1,18 @@
+public class PrefixSum {
+    public boolean[] answerQueries(int[] nums, int[][] queries, int limit) {
+        int[] prefix = new int[nums.length];
+        prefix[0] = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            prefix[i] = prefix[i - 1] + nums[i];
+        }
+
+        boolean[] ans = new boolean[queries.length];
+        for (int i = 0; i < queries.length; i++) {
+            int x = queries[i][0], y = queries[i][1];
+            int current = prefix[y] - prefix[x] + nums[x];
+            ans[i] = current < limit;
+        }
+        return ans;
+    }
+}
