bd317657b24e428b87498df42cd9322854ca9c05
UrmatBolotbek <urmar9797@gmail.com>
2025-02-07 19:45:30 +0100

    repeat binary search

---
+++ src/BinarySearch.java
@@ -0,0 +1,24 @@
+public class BinarySearch {
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int target = 4;
+        System.out.println(binarySearch(array, target));
+    }
+
+    public static int binarySearch(int[] array, int target) {
+        int left = 0;
+        int right = array.length - 1;
+
+        while (left <= right) {
+            int middle = left + (right - left) / 2;
+            if (array[middle] == target) {
+                return middle;
+            } else if (array[middle] < target) {
+                left = middle + 1;
+            } else {
+                right = middle - 1;
+            }
+        }
+        return -1;
+    }
+}
