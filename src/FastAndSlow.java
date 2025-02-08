6ab549a8dffbede8a14d6c4158a484fe08ad6a89
UrmatBolotbek <urmar9797@gmail.com>
2025-02-08 15:58:50 +0100

    add ListNode, FastAndSlow and ReverseLinkedList

---
+++ src/FastAndSlow.java
@@ -0,0 +1,12 @@
+public class FastAndSlow {
+    public int findMiddle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return slow.val;
+    }
+}
